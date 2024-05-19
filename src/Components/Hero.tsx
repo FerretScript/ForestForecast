20c8801bc53e8e77acf1f88e6a27b4067bad9541
Y0z64 <yairprogrammer@gmail.com>
2024-05-18 20:33:27 -0600

    Paralax

--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -1,32 +1,22 @@
-import { Link } from "wouter";
+ import { useScroll } from "framer-motion";
 import Header from "./Header";
+import Paralax from "./Paralax";
+import { useRef } from "react";
 
 export default function Hero() {
+  const ref = useRef(null);
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start start", "end start"],
+  });
   return (
-    <div className="flex h-screen w-screen flex-col items-center justify-start">
+    <div
+      ref={ref}
+      className="flex h-screen w-screen flex-col items-center justify-start"
+    >
       <Header />
-      <div className="relative flex h-full w-full items-center justify-center pb-28">
-        <div className="absolute left-0 top-0 -z-10 h-[60rem] w-screen bg-[#606C38] heropattern-topography-dgreen/35" />
-        <div className="z-0 -mr-10 flex h-[37rem] w-fit flex-col items-center justify-start rounded-bl-[4rem] rounded-tr-[4rem] bg-white p-10 pr-20 text-[7rem] font-[500]">
-          <div className="block text-left font-serif tracking-tight text-dgreen">
-            Forecasting the <br /> Future of{" "}
-            <span className="inline text-lgreen">Forests</span>
-          </div>
-          <div className="flex h-full w-full items-center justify-center">
-            <Link
-              href="/simulator"
-              className="rounded-3xl bg-lgreen px-5 flex items-center justify-center hover:scale-105 transition-all duration-200 text-center text-[4rem] tracking-wide"
-            >
-              <a>Start</a>
-            </Link>
-          </div>
-        </div>
-        <img
-          src="https://images.unsplash.com/photo-1425913397330-cf8af2ff40a1?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          alt="forest"
-          className="z-20 -ml-5 h-[33rem] w-[44rem] rounded-br-[4rem] rounded-tl-[4rem] object-cover"
-        />
-      </div>
+      <Paralax scrollYProgress={scrollYProgress} />
     </div>
-  );
+  );  
 }
---
+++ src/Components/Paralax.tsx
@@ -0,0 +1,89 @@
+import { motion, MotionValue, useTransform } from "framer-motion";
+import { Link } from "wouter";
+
+type Props = {
+  scrollYProgress: MotionValue<number>;
+};
+
+export default function Paralax({ scrollYProgress }: Props) {
+  const backgroundhelper = useTransform(scrollYProgress, [0, 1], ["80%", "0%"]);
+  const layer4Y = useTransform(scrollYProgress, [0, 1], ["-1%", "20%"]);
+  const layer3Y = useTransform(scrollYProgress, [0, 1], ["-1%", "5%"]);
+  const layer2Y = useTransform(scrollYProgress, [0, 1], ["-2%", "-10%"]);
+  const layer1Y = useTransform(scrollYProgress, [0, 1], ["-5%", "-25%"]);
+
+  const textY = useTransform(scrollYProgress, [0, 1], ["0%", "200%"]);
+
+  return (
+    <div className="relative grid h-full w-screen place-items-center overflow-hidden">
+      <motion.div
+        style={{ y: textY }}
+        className="relative z-30 mb-[350px] flex flex-col items-center justify-center text-5xl font-bold text-white sm:text-8xl md:text-9xl lg:text-[9.5rem]"
+      >
+        <span className="mb-5 select-none">
+          Forest<p className="inline text-dgreen">Forecast</p>
+        </span>
+        <Link href="/simulator">
+          <motion.a
+            whileHover={{ scale: 1.15 }}
+            whileTap={{scale: 0.95}}
+            className="flex cursor-pointer items-center justify-center rounded-[2rem] bg-dgreen px-6 pb-4 pt-3 text-center text-[4rem] select-none"
+          >
+            Start
+          </motion.a>
+        </Link>
+      </motion.div>
+      {/* Background */}
+      <motion.div
+        className="pointer-events-none absolute inset-0 z-0 bg-red-400"
+        style={{
+          backgroundImage: `url(./forest.png)`,
+          backgroundPosition: "bottom",
+          backgroundSize: "cover",
+        }}
+      />
+      <motion.div
+        className="pointer-events-none absolute inset-0 z-10 "
+        style={{
+          backgroundImage: `url(./layer4.png)`,
+          backgroundPosition: "bottom",
+          backgroundSize: "cover",
+          y: layer4Y,
+        }}
+      />
+      <motion.div
+        className="pointer-events-none absolute inset-0 z-20"
+        style={{
+          backgroundImage: `url(./layer3.png)`,
+          backgroundPosition: "bottom",
+          backgroundSize: "cover",
+          y: layer3Y,
+        }}
+      />
+      <motion.div
+        className="pointer-events-none absolute inset-0 z-30"
+        style={{
+          backgroundImage: `url(./layer2.png)`,
+          backgroundPosition: "bottom",
+          backgroundSize: "cover",
+          y: layer2Y,
+        }}
+      />
+      <motion.div
+        className="pointer-events-none absolute inset-0 z-40"
+        style={{
+          backgroundImage: `url(./layer1.png)`,
+          backgroundPosition: "bottom",
+          backgroundSize: "cover",
+          y: layer1Y,
+        }}
+      />
+      <motion.div
+        className="pointer-events-none absolute bottom-0 z-40 h-72 w-full bg-[#0b1e25]"
+        style={{
+          y: backgroundhelper,
+        }}
+      />
+    </div>
+  );
+}
